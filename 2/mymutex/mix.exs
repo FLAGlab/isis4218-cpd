15f067f46444205a0f37f455a2b5c9709afb3649
ncardozo <n.cardozo@uniandes.edu.co>
2024-02-05 15:21:16 -0500

    recompiling mix project
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- 2/mymutex/mix.exs
+++ 2/mymutex/mix.exs
@@ -5,7 +5,7 @@ defmodule MyMutex.MixProject do
     [
       app: :mymutex,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,7 +23,7 @@ defmodule MyMutex.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:mutex, "~>1.0.0"}
+      {:mutex, "~>2.0"}
     ]
   end
 end
