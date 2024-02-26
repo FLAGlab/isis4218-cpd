6c78c7276f1e4e1b02fee6c0ee330c45e91ddc15
ncardozo <n.cardozo@uniandes.edu.co>
2024-02-26 09:10:32 -0500

    updating mix project
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- 5/csp/mix.exs
+++ 5/csp/mix.exs
@@ -5,7 +5,7 @@ defmodule CSP.MixProject do
     [
       app: :csp,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
